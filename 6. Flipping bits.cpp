21b4185c98f6d8b75bdaf27a84e073de7f84406e
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-02 21:17:18 +0600

    ---

--- 6. Flipping bits.cpp
+++ 6. Flipping bits.cpp
@@ -4,10 +4,64 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
  
-    unsigned int deciNum;
-    cin>> deciNum;
-    bitset<32> binaryNum(deciNum);
-    
-    cout<<binaryNum <<endl; 
+    int t;
+    cin>> t;
+    while(t>0){
+        unsigned int deciNum,t;
+        cin>> deciNum;
+        
+        // cout<<deciNum <<endl; 
+        bool DtoB[32] = {0};
+        
+        for(int i=0;i<32;i++){
+            t = deciNum/2;
+            DtoB[31-i] = deciNum%2;
+            deciNum = t;
+            
+        }
+        // if (deciNum > 0) {
+                
+        //     }
+        
+        
+        for(int i=0;i<32;i++){
+            cout<<DtoB[i];
+        }
+        cout<<endl; 
+        
+        for(int i=0;i<32;i++){
+            if(DtoB[i]==0){
+                DtoB[i]=1;
+            }
+            else{
+                DtoB[i]=0;
+            }
+        }
+        for(int i=0;i<32;i++){
+            cout<<DtoB[i];
+        }
+        cout<<endl; 
+        
+        for(int i=0;i<32;i++){
+            deciNum = deciNum + (DtoB[i]*pow(2,i));
+        }
+        cout<<deciNum <<endl; 
+        // deciNum = DtoB.to_ulong();
+        // cout<<deciNum <<endl; 
+        t--;
+    }
     return 0;
 }
+
+// // cout<<deciNum <<endl; 
+
+//         bitset<32> binaryNum(deciNum);
+        
+//         // cout<<binaryNum <<endl; 
+        
+//         for(int i=0;i<32;i++){
+//             binaryNum[i]=!binaryNum[i];
+//         }
+//         // cout<<binaryNum <<endl; 
+//         deciNum = binaryNum.to_ulong();
+//         cout<<deciNum <<endl; 
--- demo.cpp
+++ demo.cpp
@@ -4,25 +4,44 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
  
-    int n;
-    cin>>n;
-
     int x;
-    int indexArray[101] ={0}; 
-    //array input
-    for(int i=0;i<n;i++){
-        cin>>x;
-        indexArray[x]++;
-    }
-
-    for(int i=0;i<n;i++){
-        if(indexArray[i]==1){
-            cout<<i<<endl;
+    cin>> x;
+    while(t>0){
+        unsigned int deciNum,t;
+        cin>> deciNum;
+        
+        // cout<<deciNum <<endl; 
+        bool DtoB[32] = {0};
+        
+        for(int i=0;i<32;i++){
+            t = deciNum/2;
+            DtoB[31-i] = deciNum%2;
+            if(deciNum%2==0){
+               DtoB[31-i] = 1; 
+            }
+            else{
+                DtoB[31-i] = 0;
+            }
+            deciNum = t;
+            // cout<<DtoB[31-i]<<endl;
+            // cout<<deciNum;  
         }
+        
+        
+        // for(int i=0;i<32;i++){
+        //     if(DtoB[i]==0){
+        //         DtoB[i]=1;
+        //     }
+        //     else{
+        //         DtoB[i]=0;
+        //     }
+        // }
+        
+        for(int i=0;i<32;i++){
+            deciNum = deciNum + (DtoB[i]*pow(2,i));
+        }
+        cout<<deciNum <<endl; 
+        x--;
     }
-    
-    cout<<"Hello"<<endl;
- 
- 
     return 0;
 }
