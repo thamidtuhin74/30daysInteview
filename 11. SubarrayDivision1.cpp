344177f6b72dd0808c5d0572137257ef7ea3f689
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-05 16:33:50 +0600

    10 sub Array Div1- try1

---
+++ 11. SubarrayDivision1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n,d,m;
+    cin>>n;
+    int s[n];
+    for(int i=0;i<n;i++){
+        cin>>s[i];
+    }
+    cin>>d;
+    cin>>m;
+    int result=0;
+    for(int i=0;i<n;i++){
+        if(s[i]+s[i+1]==d){
+            result++;
+        }
+        if(i==n-1 && s[i]==d){
+            result++;
+        }
+    }
+    cout<<result<<endl;
+ 
+ 
+    return 0;
+}
