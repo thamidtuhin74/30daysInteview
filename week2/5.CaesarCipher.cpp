36fba58b60d774806d81c250236a396f3968cc79
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-08 23:47:30 +0600

    week2- Caesar Cipher - input data

---
+++ week2/5.CaesarCipher.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int n,k;
+    cin>>n;
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    string inputString;
+
+    getline(cin, inputString);
+
+    cout<<"Input String : "<<inputString<<endl;
+    
+    cout<<n<<endl;
+ 
+ 
+    return 0;
+}
