03f6297a7287fb81e188eb64cb3481f5ce3655cd
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-01-09 02:57:02 +0600

    CaeserCiper - sloved

--- week2/5.CaesarCipher.cpp
+++ week2/5.CaesarCipher.cpp
@@ -10,11 +10,50 @@ int main(){
     string inputString;
 
     getline(cin, inputString);
+    cin>>k;
 
-    cout<<"Input String : "<<inputString<<endl;
-    
-    cout<<n<<endl;
+
+    string outputStr = "";
+    k = k%26;
+    for(int i=0;i<n;i++){
+        if((inputString[i] >= 65 && inputString[i]<= 90)||(inputString[i] >= 97 && inputString[i]<= 122)){
+            // cout<<"Hello "<<endl;
+            int ascValue = inputString[i];
+
+            if((inputString[i] >= 65 && inputString[i]<= 90)){
+                if((ascValue<= 90-k)){
+                    ascValue = ascValue + k;
+                    outputStr.push_back(static_cast<char>(ascValue));
+                }
+                else{
+                    ascValue = 65 + (ascValue+k-1-90);
+                    outputStr.push_back(static_cast<char>(ascValue));
+                }
+            }
+            else{
+                if((ascValue<= 122-k)){
+                    ascValue = ascValue + k;
+                    outputStr.push_back(static_cast<char>(ascValue));
+                }
+                else{
+                    ascValue = 97 + (ascValue+k-1-122);
+                    outputStr.push_back(static_cast<char>(ascValue));
+                }
+            }    
+        }else{
+            outputStr.push_back(inputString[i]);  
+        }
+    }
+
+    cout<<outputStr<<endl;
  
  
     return 0;
 }
+
+// Input (stdin)
+// 38
+// Always-Look-on-the-Bright-Side-of-Life
+// 5
+// Expected Output
+// Fqbfdx-Qttp-ts-ymj-Gwnlmy-Xnij-tk-Qnkj
