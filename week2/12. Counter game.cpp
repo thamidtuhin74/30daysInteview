46349429f8ad9b37a0f6c58a2770945aea22e363
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-02-03 05:37:50 +0600

    Get partial error

--- week2/12. Counter game.cpp
+++ week2/12. Counter game.cpp
@@ -1,5 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+// void notPowerOf2(long long int n, int count){
+//     count++;
+//     long long int newX = n>>1;
+//     // n = newX+1;
+    
+//     if (n == 1) {
+//         if (count % 2 == 0) {
+//             cout << "Richard" << endl;
+//         } else {
+//             cout << "Louise" << endl;
+//         }
+//     } else {
+//         notPowerOf2(n, count);
+//     }
+//     cout<<""<<endl;
+// }
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -7,10 +24,51 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int n;
+        long long int n;
+        int noOfbit;
         cin>>n;
+        noOfbit = log2(n)+1;
+        int count = 0;
+        // if(noOfbit%2==0){
+        //     cout<<"Richard"<< noOfbit<<endl;
+        // }else{
+        //     cout<<"Louise"<< noOfbit<<endl;
+
+        // }
+
+        // if(noOfbit==1){
+        //     cout<<"Richard"<<endl;
+        // }
+        // else{
+        //     int x = n & (n-1);
+        //     if(x==0){
+        //         if(noOfbit%2==0){
+        //             cout<<"Richard"<<endl;
+        //         }else{
+        //             cout<<"Louise"<<endl;
+        //         }
+        //     }
+        //     else{
+        //         notPowerOf2(n, count);
+                
+        //     } 
+        // }
+
+            int x = n & (n-1);
+        if(x == 0){
+            cout<<"Power of 2"<<endl;
+
+            }
+        else{
+            cout<<"Not Power of 2"<<endl;
+            int newX = n>>1;
+            cout<<"New X: "<<newX<<endl;
+            cout<<"n - newX : "<<(n - newX)-newX<<endl;
+
+
+
+            }
     }
-    cout<<t<<endl;
  
  
     return 0;
