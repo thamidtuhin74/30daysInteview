a2c437da0cd30212df6835bdc1d16d8a1c8dfeea
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2024-02-04 21:10:56 +0600

    partially solveed-1 test case have issue

--- week2/12. Counter game.cpp
+++ week2/12. Counter game.cpp
@@ -1,22 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// void notPowerOf2(long long int n, int count){
-//     count++;
-//     long long int newX = n>>1;
-//     // n = newX+1;
+void notPowerOf2(unsigned long long int n, int count){
+    if (n == 1) {
+        if (count % 2 == 0) {
+            cout << "Richard" << endl;
+        } else {
+            cout << "Louise" << endl;
+        }
+    }
+    else{
+        unsigned long long int bits = log2(n)+0;
+        // n = newX+1;
+        // cout << "n = " <<n<< endl;
+        // cout << "count = " <<count<< endl;
+
+        int x = n & (n-1);
+        if(x==0){
+            count += bits;
+            if(count%2==0){
+                cout<<"Richard"<<endl;
+            }else{
+                cout<<"Louise"<<endl;
+            }
+        }
+        else{
+            n = n - pow(2, bits);
+            count++;
+            // unsigned long long int newX = n>>1;
+            notPowerOf2(n, count);   
+        } 
+    }
     
-//     if (n == 1) {
-//         if (count % 2 == 0) {
-//             cout << "Richard" << endl;
-//         } else {
-//             cout << "Louise" << endl;
-//         }
-//     } else {
-//         notPowerOf2(n, count);
-//     }
-//     cout<<""<<endl;
-// }
+
+}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -24,50 +41,31 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        long long int n;
+        unsigned long long int n;
         int noOfbit;
         cin>>n;
-        noOfbit = log2(n)+1;
+        noOfbit = log2(n)+0;
         int count = 0;
-        // if(noOfbit%2==0){
-        //     cout<<"Richard"<< noOfbit<<endl;
-        // }else{
-        //     cout<<"Louise"<< noOfbit<<endl;
 
-        // }
-
-        // if(noOfbit==1){
-        //     cout<<"Richard"<<endl;
-        // }
-        // else{
-        //     int x = n & (n-1);
-        //     if(x==0){
-        //         if(noOfbit%2==0){
-        //             cout<<"Richard"<<endl;
-        //         }else{
-        //             cout<<"Louise"<<endl;
-        //         }
-        //     }
-        //     else{
-        //         notPowerOf2(n, count);
-                
-        //     } 
-        // }
-
-            int x = n & (n-1);
-        if(x == 0){
-            cout<<"Power of 2"<<endl;
-
-            }
+        if(noOfbit==1){
+            cout<<"Richard"<<endl;
+        }
         else{
-            cout<<"Not Power of 2"<<endl;
-            int newX = n>>1;
-            cout<<"New X: "<<newX<<endl;
-            cout<<"n - newX : "<<(n - newX)-newX<<endl;
-
-
-
+            int x = n & (n-1);
+            if(x==0){
+                if(noOfbit%2==0){
+                    cout<<"Richard"<<endl;
+                }else{
+                    cout<<"Louise"<<endl;
+                }
             }
+            else{
+                notPowerOf2(n, count);
+                
+            } 
+
+        }
+
     }
  
  
